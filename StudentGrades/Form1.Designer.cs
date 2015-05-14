27b18a611b92b3373f1c78be0ce4a9d5b2ce2300
vimalabhi89 <vimalabhi89@gmail.com>
2015-05-14 16:54:45 -0400

    Assignment Files
    Database Folder: Database Backup
    Project Folder: StudentGrades

---
+++ StudentGrades/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace StudentGrades
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gvStudentInfo = new System.Windows.Forms.DataGridView();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.gvAssignmentInfo = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gvStudentInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvAssignmentInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(412, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Details";
+            // 
+            // gvStudentInfo
+            // 
+            this.gvStudentInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvStudentInfo.Location = new System.Drawing.Point(287, 105);
+            this.gvStudentInfo.Name = "gvStudentInfo";
+            this.gvStudentInfo.RowTemplate.Height = 24;
+            this.gvStudentInfo.Size = new System.Drawing.Size(466, 184);
+            this.gvStudentInfo.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(456, 318);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 17);
+            this.linkLabel1.TabIndex = 2;
+            // 
+            // gvAssignmentInfo
+            // 
+            this.gvAssignmentInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvAssignmentInfo.Location = new System.Drawing.Point(104, 485);
+            this.gvAssignmentInfo.Name = "gvAssignmentInfo";
+            this.gvAssignmentInfo.RowTemplate.Height = 24;
+            this.gvAssignmentInfo.Size = new System.Drawing.Size(846, 248);
+            this.gvAssignmentInfo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 446);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Assignment-wise Student Grading";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(147, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Enter Student Name:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(393, 364);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(222, 27);
+            this.txtName.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(652, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarning.ForeColor = System.Drawing.Color.Red;
+            this.lblWarning.Location = new System.Drawing.Point(393, 430);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(398, 20);
+            this.lblWarning.TabIndex = 8;
+            this.lblWarning.Text = "No Data Found for the Selected Search!!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 776);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gvAssignmentInfo);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.gvStudentInfo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gvStudentInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvAssignmentInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gvStudentInfo;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.DataGridView gvAssignmentInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblWarning;
+    }
+}
+
---
+++ StudentGrades/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGrades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            populateStudentInfo();
+        }
+
+        public void populateStudentInfo()
+        {
+            gvAssignmentInfo.Visible = false;
+            label2.Visible = false;
+            lblWarning.Visible = false;
+            DataTable dataToReturn = new DataTable();
+            string connectionStr = "Server=localhost; Database=Student; Integrated Security=True;";
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionStr);
+
+                SqlCommand cmd = new SqlCommand("prGetStudentInfo", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dataToReturn);
+                gvStudentInfo.DataSource = dataToReturn;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String studentName = txtName.Text.ToString();
+            DataTable dataToReturn_AssignmentInfo = new DataTable();
+            string connectionStr = "Server=localhost; Database=Student; Integrated Security=True;";
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionStr);
+
+                SqlCommand cmd = new SqlCommand("prSearchStudentAssignmentWiseInfo", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@StudentName", studentName));
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dataToReturn_AssignmentInfo);
+
+                if (dataToReturn_AssignmentInfo.Rows.Count > 0)
+                {
+                    gvAssignmentInfo.DataSource = dataToReturn_AssignmentInfo;
+                    gvAssignmentInfo.Visible = true;
+                    label2.Visible = true;
+                    lblWarning.Visible = false;
+                }
+                else
+                {
+                    lblWarning.Visible = true;
+                    gvAssignmentInfo.Visible = false;
+                    label2.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
